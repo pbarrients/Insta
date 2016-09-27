05dafcd6318caefb13b590dbbb73fcb0e848c3e9
Elihu Cruz <ea_2305@hotmail.com>
2016-09-26 19:53:36 -0500

    First commit

---
+++ Instagram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Instagram.Sources;
+
+namespace Instagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            InstaHelper con = new InstaHelper();
+            con.GetAccessToken();
+            //con.getComments();
+
+        }
+    }
+}
---
+++ Instagram/Sources/Instagram.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Net;
+
+namespace Instagram.Sources
+{
+    class InstaHelper
+    {
+
+        //private string ClientID = "ab21c51375e44cc48123400341094120";
+        //public string ClientSecret = "88a8a586ae8e4978accf312133bb06d8";
+        //private string RedirectURI = "http://localhost";
+        //private string code = "30aec5a2cdde4ca7a76a05c6131f2aa9";
+        //private string Token = "3959901571.ab21c51.19f116295fb743a4aadcc6c0bd5d28f2";
+        //3959901571.ab21c51.19f116295fb743a4aadcc6c0bd5d28f2
+
+        public async Task<string> GetAccessToken()
+        {
+
+
+            String OAuthConsumerSecret = "J37DU9A32JMqqD3RoJc8kEkvsR0zn29OtDNcAMy6r53VEbyrKm";
+            String OAuthConsumerKey = "2uOSZWoBGOND11pfU6UH0oAOr";
+            var httpClient = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://api.twitter.com/oauth2/token ");
+            var customerInfo = Convert.ToBase64String(new UTF8Encoding().GetBytes(OAuthConsumerKey + ":" + OAuthConsumerSecret));
+            request.Headers.Add("Authorization", "Basic " + customerInfo);
+            request.Content = new StringContent("grant_type=client_credentials", Encoding.UTF8, "application/x-www-form-urlencoded");
+
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+
+            string json = await response.Content.ReadAsStringAsync();
+            var serializer = new JavaScriptSerializer();
+            dynamic item = serializer.Deserialize<object>(json);
+            return item["access_token"];
+
+            //string oauthUrl = string.Format("https://api.instagram.com/oauth/authorize/?client_id={0}&redirect_uri={1}&response_type=token", ClientID,RedirectURI);
+            //WebClient client = new WebClient();
+
+            //var accessToken = client.DownloadString(oauthUrl);
+
+            //return accessToken;
+
+        }
+
+
+        public async Task<dynamic> getComments(string accessToken = null)
+        {
+
+
+            if (accessToken == null)
+            {
+                accessToken = await GetAccessToken();
+            }
+
+
+            // https://api.instagram.com/v1/media/{media-id}/comments?access_token=ACCESS-TOKEN
+            var requestComments = "https://api.instagram.com/v1/media/{media-id}/comments?access_token=3959901571.ab21c51.19f116295fb743a4aadcc6c0bd5d28f2";
+            WebClient httpClient = new WebClient();
+            var result = httpClient.DownloadString(requestComments);
+
+            dynamic userinfo = JsonConvert.DeserializeObject(result);
+            return userinfo;
+        }
+
+
+    }
+
+
+}
